ea9fcc1268d1d8b23fd76da1036bd84b675d3d4d
Artemis Papanikolaou <artemispapanik@gmail.com>
2020-05-20 16:17:55 +0100

    Added a new bike object, and working method, unit test passing

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,5 +1,11 @@
 require 'bike'
 
 describe Bike do
-  
+  subject { bike }
+
+  context "responds to method working" do
+    let(:bike) { Bike.new }
+    it { should respond_to :working?}
+  end
+
 end
