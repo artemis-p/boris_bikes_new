417172b74f9349a37257b719665cd0fdda25213e
Artemis Papanikolaou <artemispapanik@gmail.com>
2020-05-31 17:30:50 +0100

    User can dock the bike and sees a docked bike

--- spec/feature_test/feature_test.rb
+++ spec/feature_test/feature_test.rb
@@ -1,4 +1,9 @@
 require './lib/docking_station.rb'
+require './lib/bike.rb'
+bike = Bike.new
 docking_station = DockingStation.new
 bike = docking_station.release_bike
 bike.working?
+docking_station.dock(bike)
+docking_station.bike
+
