a31aa18322d28b4cb3250c985f3bacc9c61e5897
Artemis Papanikolaou <artemispapanik@gmail.com>
2020-05-31 23:11:34 +0100

    Debugging release_bike method

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -8,8 +8,8 @@ class DockingStation
   end
 
   def release_bike
-    fail 'No bikes available' if bikes.empty?
-    @bike.pop
+    fail 'No bikes available' if @bikes.empty?
+    @bikes.pop
   end
 
   def dock(bike)
