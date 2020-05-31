0e1863c4f826de741e0e404c72741aad832f4fa4
Artemis Papanikolaou <artemispapanik@gmail.com>
2020-05-31 23:09:30 +0100

    Added default capacity of 20 bikes

--- spec/feature_test/feature_test.rb
+++ spec/feature_test/feature_test.rb
@@ -2,6 +2,8 @@ require './lib/docking_station.rb'
 require './lib/bike.rb'
 bike = Bike.new
 docking_station = DockingStation.new
+20.times { docking_station.dock Bike.new }
+docking_station.dock Bike new
 bike = docking_station.release_bike
 bike.working?
 docking_station.dock(bike)
